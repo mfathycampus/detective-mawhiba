8352c0768fc380ea493356526bd48696e28dd767
mfathycampus <49683294+mfathycampus@users.noreply.github.com>
2026-06-03 12:24:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify, session
 import random, time, json, os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 app.secret_key = os.environ.get("SECRET_KEY", "mawhiba-detective-2026")
 
 # ══════════════════════════════════════════
